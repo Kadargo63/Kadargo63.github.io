c2b9399ddc1f4e4afc92d06ffe45e486cb77a470
Robert Isaacs <robertisaacs87@yahoo.com>
2022-01-15 18:22:29 -0600

    save spot

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import ContactForm from './components/Contact'
+import ContactForm from './components/Contact/ContactForm'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "./App.css";
